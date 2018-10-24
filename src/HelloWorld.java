ff4c8845171373fb83d1c80260ddd72e85050fc0
js16968 <js16968@essex.ac.uk>
2018-10-24 09:53:39 +0100

    Created HelloWorld.java and added println

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public HelloWorld() {
+        System.out.println("Justas Stromilas was here.");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+}
