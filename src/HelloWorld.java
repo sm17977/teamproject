22d6b4c32ce357b26c304be25ae62a0553def249
sm17977 <sm17977@essex.ac.uk>
2018-10-31 09:11:47 +0000

    Added name to Hello World class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Justas Stromilas was here.");
         System.out.println("Finlay Georgiou-Young was here.");
+        System.out.println("Sean Mcleod was here.");
     }
 }
