484558d612bdf01a5308d67ae7c0564080083f29
Justas <16074672+stromjust@users.noreply.github.com>
2018-11-05 15:41:15 +0000

    Added simple Frame class that extends JFrame and call it in main class.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,29 @@
+import javax.swing.*;
+
 public class HelloWorld {
     public static void main(String[] args) {
+        //printNames();
+
+        Frame frame = new Frame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(3);
+
+    }
+
+    static void printNames() {
         System.out.println("Justas Stromilas was here.");
         System.out.println("Finlay Georgiou-Young was here.");
         System.out.println("Sean Mcleod was here.");
-        System.out.println("Riaz Philippe was here."); 
+        System.out.println("Riaz Philippe was here.");
         System.out.println("Cameron Cunningham was here.");//Server Test Pull & Push Complete
+    }
+}
+
+// Build frame in this class
+class Frame extends JFrame {
+    private int size = 400;
+    public Frame(){
+        setTitle("Default Frame");
+        setSize(size, size);
     }
 }
