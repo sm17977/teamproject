a10be16ebdd541892ac784371b0c2514b6a036b4
Justas <16074672+stromjust@users.noreply.github.com>
2018-11-22 14:54:18 +0000

    Moved all .java files in PersonalFinanceApp package, updated name of main .java file from HelloWorld.java to Main.java, made an example in main function showing how to use Market.java

---
+++ src/pesonalFinanceApp/Market.java
@@ -0,0 +1,27 @@
+package pesonalFinanceApp;
+
+import pl.zankowski.iextrading4j.api.stocks.Chart;
+import pl.zankowski.iextrading4j.api.stocks.ChartRange;
+import pl.zankowski.iextrading4j.client.IEXTradingClient;
+import pl.zankowski.iextrading4j.client.rest.request.stocks.*;
+
+import java.util.List;
+
+
+class Market {
+    final private IEXTradingClient tradingClient = IEXTradingClient.create();
+    Market() { }
+    // Current stock price of company
+    // Symbols for companies are listed here:
+    // https://iextrading.com/trading/eligible-symbols/
+
+    // List of quotes for company for last 5 years
+    List<Chart> getStockPrice(String symbol) {
+        final List<Chart> chartList = tradingClient.executeRequest(new ChartRequestBuilder()
+                .withChartRange(ChartRange.FIVE_YEARS)
+                .withSymbol(symbol)
+                .build());
+        return chartList;
+    }
+}
+
