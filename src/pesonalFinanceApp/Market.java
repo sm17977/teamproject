05ceac7d2ad2b951d3bf9671fd59a7bb522b3ce8
sm17977 <44192194+sm17977@users.noreply.github.com>
2018-11-28 22:35:53 +0000

    Commented and tidied Market Class

--- src/pesonalFinanceApp/Market.java
+++ src/pesonalFinanceApp/Market.java
@@ -19,29 +19,27 @@ import java.util.List;
 
 
 class Market {
+    //String symbol is retrieved from the GUI from the Company text field, and is used to validate against list of all IEX Stock Symbols
     private String symbol;
+    //int ShareCount Holds the quantity of shares for the Company that is inputted into the GUI
     private int shareCount;
+    //Not used yet
     private int totalShareVal;
+    //int bankBalance holds the value of the clients bank balance which is retrieved from the GUI text field bank balance
     private int bankBalance;
-    //Get symbol string from GUI
+    //tradingClient is an IEXTrading Client object that allows information to be retrieved using the IEX API
     final private IEXTradingClient tradingClient = IEXTradingClient.create();
-    //List containing all IEX symbols in
+    //exchangeSymbolList is a list of type "ExchangeSymbol" that contains all valid IEX symbols
     final List<ExchangeSymbol> exchangeSymbolList = tradingClient.executeRequest(new SymbolsRequestBuilder().build());
 
-
-
-
-
-
+    //Market constructor gets variables from GUI input, so can be accessed and used in method generateChart
     Market(String symbol, int shareCount, int bankBalance) {
         this.symbol = symbol;
         this.shareCount = shareCount;
         this.bankBalance = bankBalance;
     }
 
-
-
-    //Generates current day share value data for the specified company
+    //Generates current day share value data for the company specified in the GUI input, uses IEX Chart type and converts to ArrayList
     ArrayList generateChart (ArrayList list){
         final List<Chart> chartList = tradingClient.executeRequest(new ChartRequestBuilder()
                 .withChartRange(ChartRange.FIVE_YEARS)
@@ -54,8 +52,6 @@ class Market {
         return list;
     }
 
-
-
     //Convert the List of type <ExchangeSymbol> to ArrayList so it can be used to compare strings
     ArrayList getSymbolList(ArrayList list){
         for(ExchangeSymbol symbol: exchangeSymbolList){
@@ -63,8 +59,6 @@ class Market {
         }
         return list;
     }
-
-
 
     //Check if user entered Company is an IEX Symbol
     boolean validateSymbol (String symbol, ArrayList list){
