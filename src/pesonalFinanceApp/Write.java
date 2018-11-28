5dfc45fc934b4d1a3537d4f42ca7166a41d17f19
sm17977 <44192194+sm17977@users.noreply.github.com>
2018-11-28 08:36:54 +0000

    Added Write class to write data to CSV File

---
+++ src/pesonalFinanceApp/Write.java
@@ -0,0 +1,42 @@
+package pesonalFinanceApp;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+//Class to write CSV File
+public class Write {
+
+    private String FileName = "outputData.csv";
+
+
+
+    public Write(){
+    }
+
+
+     void writeToFile (ArrayList list){
+         try {
+             FileWriter fileWriter = new FileWriter(FileName, true);
+             File f = new File(FileName);
+             PrintWriter printWriter = new PrintWriter(fileWriter);
+
+             printWriter.println(list.get(list.size() -1));
+             printWriter.close();
+             System.out.println("Writing to: " + f.getAbsolutePath());
+
+         }
+         catch (IOException e){
+             System.out.println("File creation failed.");
+         }
+
+
+
+
+     }
+
+
+
+}
