60902ba94aa4a062ddbd53bfec8e9a8b47f666a1
sm17977 <44192194+sm17977@users.noreply.github.com>
2019-02-10 18:48:09 +0000

    Added call to load client profiles on app start

--- src/pesonalFinanceApp/Main.java
+++ src/pesonalFinanceApp/Main.java
@@ -1,9 +1,14 @@
 package pesonalFinanceApp;
 
+import java.io.IOException;
+
+import static pesonalFinanceApp.GUI.saveClientList;
+
 public class Main {
     public static void main(String[] args) {
 
         GUI frame = new GUI();
+        frame.updateClientList();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(3);
 
