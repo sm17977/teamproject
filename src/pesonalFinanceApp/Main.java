c6ced1e97922c3dedcf8c0b6c3f3ae1fd9b2e403
Riaz Philippe <rp16181@essex.ac.uk>
2019-02-24 18:32:10 +0000

    Main Class Changelog Added /*PENDING*/ comments subject to team review. Fixed some whitespace issues.

--- src/pesonalFinanceApp/Main.java
+++ src/pesonalFinanceApp/Main.java
@@ -1,16 +1,20 @@
 package pesonalFinanceApp;
 
+/*
+PENDING - UNUSED IMPORT STATEMENT
 import java.io.IOException;
+*/
 
+/*
+PENDING - UNUSED IMPORT STATEMENT
 import static pesonalFinanceApp.GUI.saveClientList;
+*/
 
 public class Main {
     public static void main(String[] args) {
-
         GUI frame = new GUI();
         frame.updateClientList();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(3);
-
     }
 }
