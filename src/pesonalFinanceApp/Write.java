d0b1eba15e81a6c8aa9cd19fa3e959cf535efdcd
Riaz Philippe <rp16181@essex.ac.uk>
2019-02-24 18:46:08 +0000

    Write Class Changelog Updated the formatting of comments to be consistent with the rest of the program. Fixed some whitespace inconsistencies.

--- src/pesonalFinanceApp/Write.java
+++ src/pesonalFinanceApp/Write.java
@@ -6,12 +6,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-//Class to write CSV File
+//-- Class to write CSV File --
 public class Write {
     private String FileName = "dataOutput.csv";
 
-    public Write(){
-    }
+    public Write() { }
 
      void writeToFile (ArrayList list){
          try {
@@ -19,13 +18,12 @@ public class Write {
              File f = new File(FileName);
              PrintWriter printWriter = new PrintWriter(fileWriter);
 
-             //This will retrieve values from today, to view historical data use a loop
-             printWriter.println(list.get(list.size() -1));
+             // -- This will retrieve values from today, to view historical data use a loop --
+             printWriter.println(list.get(list.size() - 1));
              printWriter.close();
              System.out.println("Writing to: " + f.getAbsolutePath());
-
          }
-         catch (IOException e){
+         catch (IOException e) {
              System.out.println("File creation failed.");
          }
      }
